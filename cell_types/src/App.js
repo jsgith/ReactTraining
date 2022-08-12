b012c6f0a9c5018e5ef10961c11eb92db8ce69bb
Joao <joao_gens@hotmail.com>
2022-08-12 11:23:56 +0100

    Get data and show all the cells

--- cell_types/src/App.js
+++ cell_types/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Cell from './components/Cell';
+import data from './data';
 
 function App() {
+
+  const cells = data.map(cell => {
+    return <Cell cellName = {cell.cellName}
+                 img = {`../../images/${cell.image}`}
+                 description = {cell.description}/>
+  })
+
   return (
     <div className="App">
       <Navbar/>
-      <Cell/>
+      {cells}
     </div>
   );
 }
