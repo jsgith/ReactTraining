86c1a6db56de40ec4552fec2e7908573d81c4628
Joao <joao_gens@hotmail.com>
2022-08-16 10:56:14 +0100

    Fixed badge

--- cell_types/src/components/Cell.js
+++ cell_types/src/components/Cell.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Cell(props) {
      return (
         <div className='cell'>
-            <div className='cell--badge'>B_W</div>
             <h3 className='cell--name'>{props.cellName}</h3>
+            <div className='cell--badge'>B_W</div>
             <img  src={`../../images/${props.img}`} className='cell--img' alt='Cell'/>
             <p className='cell--description'>{props.description}</p>
         </div>
