ab98c8886bdbe8893c2c9a0759a03f002a5a8de1
Joao <joao_gens@hotmail.com>
2022-09-12 12:10:24 +0100

    added and formatted logo

--- upfor/src/App.js
+++ upfor/src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.css';
 import Pages from './pages/Pages';
 import Category from './components/Category';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 import Search from './components/Search';
+import styled from 'styled-components';
+import { SiApacheairflow } from "react-icons/si";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav>
+          <SiApacheairflow/>
+          <Logo to={"/"}>upfor</Logo>
+        </Nav>
         <Search/>
         <Category/>
         <Pages/>
@@ -16,5 +22,27 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: 'Montserrat Alternates', sans-serif;
+`;
+
+const Nav = styled.div`
+  margin-top: 1rem;
+  margin-bottom: 2rem;
+  padding: 4rem, 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+  a{
+    margin-left: 10px;
+  }
+`;
 
 export default App;
